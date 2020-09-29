06a60a896976f670f242203d647e289a99ef27a2
MFcortez <69491016+MFcortez@users.noreply.github.com>
2020-09-29 03:30:38 -0300

    Tarefa Complementar
    Tarefa referente ao dia 07/09

---
+++ POO_07_09_2020/POO_07_09_2020/Classes.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace POO_07_09_2020
+{
+	public class Classes
+	{
+		public class Retangulo
+		{
+			public double _largura;
+			public double _altura;
+
+			public double Area()
+			{
+				return _largura * _altura;
+			}
+			public double Perimetro()
+			{
+				return (_largura + _altura) * 2;
+			}
+			public double Diagonal()
+			{
+				return Math.Sqrt((_altura * _altura) + (_largura * _largura));
+			}
+
+			public override string ToString() 
+			{
+				return "Area: " + Area()
+					+ " Perimetro: " + Perimetro()
+					+ " Diagonal: " + Diagonal();
+			}
+		}
+		public class Funcionario
+		{
+			public string _nome;
+			public double _salarioBruto;
+			public double _imposto;
+
+			public double SalarioLiquido()
+			{
+				return _salarioBruto - _imposto;
+			}
+
+			public void AumentarSalario(double porcentagem)
+			{
+				_salarioBruto += _salarioBruto*(porcentagem/100);
+			}
+
+			public override string ToString()
+			{
+				return _nome
+					+ ", " + SalarioLiquido();
+			}
+
+		}
+
+		public class Aluno
+		{
+			public string _aluno;
+			public double [] _notas = new double [3];
+			
+
+			public void SetNotas()
+			{
+				for(int x = 0; x < 3; x++)
+				{
+					Console.Write("Insira a " + (x+1) + "ª nota: ");
+					_notas[x] = Double.Parse(Console.ReadLine());
+
+					if(_notas[0] > 30)
+					{
+						_notas[0] = 30;
+					}
+					else if(_notas[x] > 35)
+					{
+						_notas[x] = 35;
+					}
+				}
+			}
+
+			public double NotaFinal()
+			{
+				return _notas.Sum();
+			}
+
+			public string Situacao()
+			{
+				if(_notas.Sum() >= 60)
+				{
+					return "Aprovado";
+				}
+				else
+				{
+					return "Reprovado";
+				}
+			}
+
+			public override string ToString()
+			{
+				return _aluno
+					+ "\nNota 1: " + _notas[0]
+					+ "\nNota 2: " + _notas[1]
+					+ "\nNota 3: " + _notas[2]
+					+ "\nNota Final: " + NotaFinal()
+					+"\n" + Situacao();
+			}
+		}
+
+	}
+}
---
+++ POO_07_09_2020/POO_07_09_2020/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace POO_07_09_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha o programa a executar");
+            int x = Int32.Parse(Console.ReadLine());
+
+            if(x == 1)
+            {
+                Classes.Retangulo r;
+                r = new Classes.Retangulo();
+
+                Console.Write("Declare a largura do retangulo: ");
+                r._largura = Double.Parse(Console.ReadLine());
+                Console.Write("Declare a altura do retangulo: ");
+                r._altura = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine(r.ToString());
+            }
+            else if(x == 2)
+            {
+                Classes.Funcionario f;
+                f = new Classes.Funcionario();
+
+                Console.Write("Nome do Funcionário: ");
+                f._nome = Console.ReadLine();
+                Console.Write("Salário Bruto: ");
+                f._salarioBruto = Double.Parse(Console.ReadLine());
+                Console.Write("Imposto sobre o Salário: ");
+                f._imposto = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine(f.ToString());
+
+                Console.Write("Aumento: ");
+                f.AumentarSalario(Double.Parse(Console.ReadLine()));
+
+                Console.WriteLine(f.ToString());
+            }
+            else
+            {
+                Classes.Aluno a;
+                a = new Classes.Aluno();
+
+                Console.Write("Nome do Aluno: ");
+                a._aluno = Console.ReadLine();
+                a.SetNotas();
+                Console.WriteLine(a.ToString());
+            }
+        }
+    }
+}
